b02930b648f780c2bfd33eba1369153b90d44558
Vince Williams <williamsvm20@gmail.com>
2020-08-06 16:10:32 -0400

    Set up the server on port 7000

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const express = require('express')
+const server = require('./server')
+
+const port = 7000
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
