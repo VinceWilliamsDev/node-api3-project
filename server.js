ffdd1628d7d3985cf7979b669daeb4f3cf2966f6
Vince Williams <williamsvm20@gmail.com>
2020-08-06 17:34:02 -0400

    cannot set headers error in validateUserId

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const logger = require('./middleware/logger')
+const userRouter = require('./users/userRouter')
 
 const server = express();
-
-server.use(express.json)
 
 server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use('/api/users', userRouter)
+
+
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(`Request type: ${req.method} from ${req.originalUrl} recieved at ${new Date().toISOString()}`)
+
+  next()
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./userDb')
 
 const router = express.Router();
 
@@ -6,34 +7,48 @@ router.post('/', (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
 });
 
 router.get('/', (req, res) => {
+  db.get()
+    .then(users => {
+      if(users.length > 0) {
+        return (res.json(users))
+      } else {
+        return(res.status(404).json({error: 'users not found'}))
+      }
+    })
+    .catch(err => res.status(500).json(err))
+});
+
+router.get('/:id', validateUserId, (req, res) => {
+  res.json('is it working?')
+});
+
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   // do your magic!
 });
 
 //custom middleware
-
-function validateUserId(req, res, next) {
-  // do your magic!
+function validateUserId(req, res, next){
+  const userId = req.params.id
+  db.getById(userId)
+    .then(validUser => {
+      console.log('validating', validUser)
+      req.user = validUser
+      next()
+    })
+    .catch(err = res.status(500).json({error: 'Invalid user ID'}))
 }
 
 function validateUser(req, res, next) {
