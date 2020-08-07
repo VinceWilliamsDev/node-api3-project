f31166e9798a530d635a3d79d466dff144b31d82
Vince Williams <williamsvm20@gmail.com>
2020-08-06 20:46:28 -0400

    updated startup script and dynamic port number for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require('./server')
 
-const port = 7000
+const port = process.env.PORT || 7000
 
 server.listen(port, () => {
     console.log(`server listening on port ${port}`)
