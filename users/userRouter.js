c8a1e407c1b54f9858686816b0276a3cf2915319
Vince Williams <williamsvm20@gmail.com>
2020-08-06 20:38:59 -0400

    All endpoints are operational and middleware is working properly

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,14 +1,30 @@
 const express = require('express');
-const db = require('./userDb')
+const db = require('./userDb');
+const postsDb = require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+ db.insert({name: req.body.name})
+  .then(newUser => {
+    res.json(newUser)
+  })
+  .catch(err => res.status(500).json({error: 'Unable to add new user to database'}))
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const newPost = {
+    text: req.body.text,
+    user_id: req.params.id
+  }
+  postsDb.insert(newPost)
+    .then(post => {
+      console.log(post)
+      res.json(post)
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Error occured while adding post to the database.'})
+    })
 });
 
 router.get('/', (req, res) => {
@@ -24,39 +40,86 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  res.json('is it working?')
+  res.json(req.user)
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+  db.getUserPosts(req.params.id)
+    .then(posts => {
+      res.json(posts)
+    })
+    .catch(err => {
+      res.status(500).json({error: 'unable to retrieve the users posts at this time'})
+    })
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
-  // do your magic!
+ db.remove(req.params.id)
+  .then(records => {
+    console.log(records)
+    res.json({recordsDeleted: records})
+  })
+  .catch(err => {
+    res.status(500).json({error: 'unable to remove user'})
+  })
 });
 
-router.put('/:id', validateUserId, (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  const updatedUser = {
+    id: req.params.id,
+    name: req.body.name
+  }
+  db.update(req.params.id, updatedUser)
+    .then(count => {
+      console.log(count)
+      if (count === 1) {
+        res.json({message: 'The user was successfully updated.'})
+      } else {
+        res.status(500).json({error: 'Error while updating the user.'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Unable to update specified user'})
+    })
 });
 
 //custom middleware
 function validateUserId(req, res, next){
-  const userId = req.params.id
-  db.getById(userId)
+  db.getById(req.params.id)
     .then(validUser => {
       console.log('validating', validUser)
-      req.user = validUser
-      next()
+      if (validUser) {
+        req.user = validUser
+        next()
+      } else {
+        res.status(404).json({error: 'user not found'})
+      }
     })
-    .catch(err = res.status(500).json({error: 'Invalid user ID'}))
+    .catch(err => res.status(500).json({error: 'Invalid user ID'}))
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (req.body) {
+    if (req.body.name) {
+      next()
+    } else {
+      res.status(400).json({error: 'missing required name field'})
+    }
+  } else {
+    res.status(400).json({error: 'missing user data'})
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
-}
-
+  if (req.body) {
+    if (req.body.text) {
+      next()
+    } else {
+      res.status(400).json({error: 'missing required text field'})
+    }
+  } else {
+    res.status(400).json({error: 'missing post data'})
+  }
+  
+} 
 module.exports = router;
